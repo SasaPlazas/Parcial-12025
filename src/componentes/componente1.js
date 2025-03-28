a16758902d548de6f001febca0fe583b997c4431
SasaPlazas <saraplazasramirez@gmail.com>
2025-03-28 18:11:47 -0500

    quinto  commit

--- src/componentes/componente1.js
+++ src/componentes/componente1.js
@@ -2,12 +2,14 @@ class Componente extends HTMLElement{
 
     constructor (){
         element.attachShadow({ mode: "open" });
-    
+        element.shadowRoot();
     }
 
+
+
     connectedCallback() {
-       this.shadowRoot(['names', 'names', 'names']);
-      }
+        getAttribute(['destino', 'duracion', 'costo', 'descripcion', 'actividades', 'disponibilidad', 'img', 'calificacion','tipo', 'guia' ]);
+     }
 
     
     attributeChangedCallback(name, oldValue, newValue) {
@@ -17,8 +19,19 @@ class Componente extends HTMLElement{
 
       render(){
         this.shadowRoot.innerHTML = `
-        
-        `
+      
+        <destino>${this.destino}</destino>
+        <duracion>${this.duracion}</duracion>
+        <costo>${this.costo}</costo>
+        <descripcion>${this.descripcion}</descripcion>
+        <actividades>${this.actividades}</actividades>
+        <disponibilidad>${this.disponibilidad}</disponibilidad>
+        <img src="${this.img}" alt="${this.descripcion}">
+        <calificacion>${this.calificacion}</calificacion>
+        <tipo>${this.tipo}</tipo>
+        <guia>${this.guia}</guia>
+        `;
+
       }
 
 
